c7ef1d70486a2eafc229572a777debb17481f828
chamarthiajaykumar <ajayaj0302@gmail.com>
2023-05-02 00:05:03 +0530

    CORS policy changed to All

--- app.js
+++ app.js
@@ -8,12 +8,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
