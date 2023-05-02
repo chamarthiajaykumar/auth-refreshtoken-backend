af22cda90b25c141bda62aee85617e8d4076854f
chamarthiajaykumar <ajayaj0302@gmail.com>
2023-05-02 19:36:50 +0530

    CORS policy updated

--- app.js
+++ app.js
@@ -8,7 +8,30 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(cors());
+// CORS // Access-Control-Allow-Origin * (all users)
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
 
 app.use(express.json());
 
